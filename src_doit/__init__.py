76cd074620d9804041e28658e644c8bba75c0d79
bozo.kopic <bozo@kopic.xyz>
2021-11-02 15:12:52 +0100

    doit update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -42,8 +42,6 @@ def task_build():
         build_wheel(
             src_dir=src_py_dir,
             dst_dir=build_py_dir,
-            src_paths=list(common.path_rglob(src_py_dir,
-                                             blacklist={'__pycache__'})),
             name='hat-sbs',
             description='Hat simple binary serializer',
             url='https://github.com/hat-open/hat-sbs',
