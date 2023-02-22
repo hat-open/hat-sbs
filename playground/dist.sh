0a98e29bab14ee96ea906896386539646097e98e
bozo.kopic <bozo@kopic.xyz>
2023-02-22 18:10:31 +0100

    playground dist

--- playground/dist.sh
+++ playground/dist.sh
@@ -4,13 +4,6 @@ set -e
 
 . $(dirname -- "$0")/env.sh
 
-# TARGETS="linux_gnu_x86_64:cp310
-#          linux_gnu_aarch64:cp39
-#          linux_gnu_aarch64:cp38
-#          linux_gnu_aarch64:cp310
-#          windows_amd64:cp38
-#          windows_amd64:cp39
-#          windows_amd64:cp310"
 TARGETS="linux_gnu_x86_64:cp38
          linux_gnu_x86_64:cp39
          linux_gnu_x86_64:cp310
@@ -36,27 +29,23 @@ for TARGET in $TARGETS; do
     cp $ROOT_PATH/build/py/dist/*.whl $DIST_PATH
 done
 
-# IMAGES="build-hat-sbs:3.8-debian
-#         build-hat-sbs:3.9-debian
-#         build-hat-sbs:3.10-debian
-#         build-hat-sbs:3.8-alpine
-#         build-hat-sbs:3.9-alpine
-#         build-hat-sbs:3.10-alpine"
-# IMAGES="build-hat-sbs:3.8-debian
-#         build-hat-sbs:3.9-debian
-#         build-hat-sbs:3.8-alpine
-#         build-hat-sbs:3.9-alpine
-#         build-hat-sbs:3.10-alpine"
-IMAGES=""
+IMAGES="linux/arm/v7/build-hat-sbs:debian11-cpy3.8
+        linux/arm/v7/build-hat-sbs:debian11-cpy3.9
+        linux/arm/v7/build-hat-sbs:debian11-cpy3.10"
 
 for IMAGE in $IMAGES; do
     $PYTHON -m doit clean_all
+    PLATFORM=$(dirname $IMAGE)
     IMAGE_ID=$(podman images -q $IMAGE)
-    podman build -f $RUN_PATH/dockerfiles/$IMAGE -t $IMAGE .
+    podman build --platform $PLATFORM \
+                 -f $RUN_PATH/dockerfiles/$IMAGE \
+                 -t $IMAGE \
+                 .
     if [ -n "$IMAGE_ID" -a "$IMAGE_ID" != "$(podman images -q $IMAGE)" ]; then
         podman rmi $IMAGE_ID
     fi
     podman run --rm \
+               --platform $PLATFORM \
                -v $DIST_PATH:/hat/dist \
                -v ~/.cache/pip:/root/.cache/pip \
                -i $IMAGE /bin/sh - << EOF
